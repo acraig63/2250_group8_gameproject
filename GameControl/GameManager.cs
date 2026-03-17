3cf62cd0c8dcc5633bab435c79f2b10db86e15f2
Richard-Xu-86 <richardxubusiness@gmail.com>
2026-03-17 16:12:09 -0400

    Finalized GameManager class for the GameControl subsystem with comments throughout

--- GameControl/GameManager.cs
+++ GameControl/GameManager.cs
@@ -2,41 +2,56 @@ namespace GameControl;
 
 using UnityEngine;
 
+// This class controls the overall game flow from start to update to end
 public class GameManager : MonoBehaviour
 {
+    // only one gameManager exists
     public static GameManager Instance;
 
+    // reference to levelManager, handles the levels
     public LevelManager levelManager;
+    
+    // tracks if game is currently running
     public bool isRunning = false;
 
+    // awake runs before start
     void Awake()
     {
+        // If no instance exists, assign this as the global instance
         if (Instance == null)
         {
             Instance = this;
+
+            // Prevent this object from being destroyed when changing scenes
             DontDestroyOnLoad(gameObject);
         }
         else
         {
+            // If another GameManager exists, destroy this one
             Destroy(gameObject);
         }
     }
-
+    
+    // start runs once
     void Start()
     {
         StartGame();
     }
 
+    // update every frame
     void Update()
     {
+        // if not running do not do anything
         if (!isRunning) return;
 
+        // if levelmanger and curent level exist, update the level
         if (levelManager != null && levelManager.GetCurrentLevel() != null)
         {
             levelManager.GetCurrentLevel().UpdateLevel();
         }
     }
 
+    // inititializes game and loads first level
     public void StartGame()
     {
         Debug.Log("Game Started");
@@ -47,11 +62,13 @@ public class GameManager : MonoBehaviour
         isRunning = true;
     }
 
+    // loads next level
     public void LoadNextLevel()
     {
         levelManager.LoadNextLevel();
     }
 
+    // ends game and prints result
     public void EndGame(bool victory)
     {
         isRunning = false;
