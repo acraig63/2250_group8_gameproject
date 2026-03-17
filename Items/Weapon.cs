c402eaeb1fc0222a60f22e18061286ff128c3157
weimikey88 <michaelwei882006@gmail.com>
2026-03-17 14:46:51 -0400

    Added Weapon Class and WeaponType enum, and made changes to Item Class - Michael Wei

---
+++ Items/Weapon.cs
@@ -0,0 +1,59 @@
+﻿namespace DefaultNamespace;
+
+public class Weapon
+{
+    // Fields and properties
+    private int _baseDamage;
+    public int BaseDamage
+    {
+        get { return _baseDamage; }
+        set
+        {
+            calculateDamage();
+        }
+    }
+
+    private String _weaponType;
+    public String WeaponType WeaponType
+    {
+        get {
+            return _weaponType; 
+        }
+        set
+        {
+            _weaponType = value;
+        }
+    }
+
+    // private string _specialEffect;
+    // public string SpecialEffect
+    // {
+    //     get { return _specialEffect; }
+    //     set { _specialEffect = value; }
+    // }
+
+    // public void applyEffect(NPC target)
+    // {
+    //     
+    // }
+
+ 
+    /**
+     * calculareDamage() Method : Calcualtes the amount of damage the weapon will deal
+     * @return: Integer, damage value
+     */
+    public int calculateDamage()
+    {
+        
+    }
+
+    /**
+     * use() Meethod: Equips the item to the player
+     * @param: Player, the player
+     */
+    public void use(Player player)
+    {
+        player.equipItem(this)
+    }
+    
+    
