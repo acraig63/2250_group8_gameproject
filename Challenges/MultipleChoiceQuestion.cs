a72e452c7d2f9a0f6582158d19421d2d6bcf2476
Richard-Xu-86 <richardxubusiness@gmail.com>
2026-03-18 11:48:43 -0400

    Finalized CodingQuestion class and subclass MultipleChoiceQuestion as well as added comments throughout

--- Challenges/MultipleChoiceQuestion.cs
+++ Challenges/MultipleChoiceQuestion.cs
@@ -2,10 +2,14 @@ namespace Challenges;
 
 using System.Collections.Generic;
 
+// Sub class of CodingQuestion class that represents a multiple choice question
+// Defines how the question works
 public class MultipleChoiceQuestion : CodingQuestion
 {
+    // List of possible choices
     public List<string> options;
 
+    // Constructor to initialize a multiple-choice question
     public MultipleChoiceQuestion(
         string question,
         List<string> options,
@@ -15,9 +19,12 @@ public class MultipleChoiceQuestion : CodingQuestion
         string hint
     ) : base(question, correctAnswer, xp, difficulty, hint)
     {
+        // Store the choices
         this.options = options;
     }
 
+    // Overrides the abstract method from CodingQuestion
+    // Checks if the player's answer is correct
     public override bool Evaluate(string playerAnswer)
     {
         return playerAnswer == correctAnswer;
