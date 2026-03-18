06a6d8ad2bcf992e26dbfcb83502044313c6495d
Richard-Xu-86 <richardxubusiness@gmail.com>
2026-03-18 11:48:43 -0400

    Finalized CodingQuestion class and subclass MultipleChoiceQuestion as well as added comments throughout

--- Challenges/CodingQuestion.cs
+++ Challenges/CodingQuestion.cs
@@ -1,13 +1,17 @@
 namespace Challenges;
 
+// base parent class for all types of questions
+// abstract base class for all coding questions
 public abstract class CodingQuestion
 {
+    // common properties shared by all subclasses
     public string questionText;
     public string correctAnswer;
     public int xpValue;
     public int difficulty;
     public string hintText;
 
+    //constructor to initialize fields
     public CodingQuestion(string question, string answer, int xp, int difficulty, string hint)
     {
         this.questionText = question;
@@ -17,13 +21,17 @@ public abstract class CodingQuestion
         this.hintText = hint;
     }
 
+    // Abstract method that must be implemented by subclasses
+    // Defines how answers are evaluated
     public abstract bool Evaluate(string playerAnswer);
 
+    // Returns question text
     public string GetQuestionText()
     {
         return questionText;
     }
 
+    // Returns question hint
     public string GetHint()
     {
         return hintText;
