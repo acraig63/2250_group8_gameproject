f2f05d6a28419064ef7e81e3040d80d4634eadfe
weimikey88 <michaelwei882006@gmail.com>
2026-03-22 23:06:44 -0400

    Added test script for Items

---
+++ Assets/Scripts/Items/ItemTest.cs
@@ -0,0 +1,26 @@
+﻿namespace DefaultNamespace;
+
+public class ItemTest
+{
+using UnityEngine;
+using DefaultNamespace;
+
+public class ItemTest : MonoBehaviour
+{
+    void Start()
+    {
+        Weapon sword = new Weapon("wpn_001", "Iron Cutlass", "A basic sword",
+            25, Rarity.Common, 10, WeaponType.Sword);
+
+        Clothing hat = new Clothing("clo_001", "Pirate Hat", "A tricorn",
+            15, Rarity.Uncommon, ClothingSlot.Head, 3);
+
+        TreasureItem key = new TreasureItem("key_001", "Brass Key", "Opens a cell",
+            0, Rarity.Rare, ItemType.Key);
+
+        Debug.Log($"Weapon: {sword.Name}, Damage: {sword.calculateDamage()}");
+        Debug.Log($"Clothing: {hat.Name}, Defense: {hat.DefenseBonus}");
+        Debug.Log($"Treasure: {key.Name}, Type: {key.Type}");
+    }
+}
+}
