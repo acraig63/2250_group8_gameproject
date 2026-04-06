1ceb778d55c67f11667b692c4f98020162a43762
Rory Craig <roryc@dyn172-30-189-179.wireless.uwo.pri>
2026-04-06 17:07:50 -0400

    added animated player functionality to stormbreaker island

--- Assets/Scripts/OpeningPage/CharacterSelectManager.cs
+++ Assets/Scripts/OpeningPage/CharacterSelectManager.cs
@@ -18,6 +18,6 @@ public class CharacterSelectManager : MonoBehaviour
 
         Debug.Log("Selected: " + selectedCharacter);
 
-        SceneManager.LoadScene("TwistedGardens");
+        SceneManager.LoadScene("StormbreakerIsland");
     }
 }
