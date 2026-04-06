04e83ce0c28ef30582db88e76e313235f3ad36f8
weimikey88 <michaelwei882006@gmail.com>
2026-04-06 18:13:15 -0400

    Moved player respawn logic after winning battle from PlayerManager to PlayerSpawner

--- Assets/Scripts/PlayerMovement/PlayerManager.cs
+++ Assets/Scripts/PlayerMovement/PlayerManager.cs
@@ -5,6 +5,7 @@ using DefaultNamespace;
 public class PlayerManager : MonoBehaviour
 {
     public Player player;
+    private GameObject _spawnedPlayer;
 
     public static string playerType = "Archer";
 
@@ -14,26 +15,6 @@ public class PlayerManager : MonoBehaviour
 
         player = new Player(selectedName, "Pirate");
 
-        Debug.Log($"HasReturnPosition: {BattleData.HasReturnPosition}, ReturningFromBattle: {BattleData.ReturningFromBattle}, Position: {BattleData.ReturnPlayerPosition}");
-
-        if (BattleData.HasReturnPosition)
-        {
-            StartCoroutine(RestoreReturnPositionNextFrame());
-            BattleData.HasReturnPosition = false;
-            BattleData.ReturningFromBattle = false;
-        }
-
         Debug.Log("Spawned player: " + selectedName);
-    }
-
-    private IEnumerator RestoreReturnPositionNextFrame()
-    {
-        yield return null;
-
-        Debug.Log("Restoring player to return position: " + BattleData.ReturnPlayerPosition);
-
-        transform.position = BattleData.ReturnPlayerPosition;
-        BattleData.HasReturnPosition = false;
-        BattleData.ReturningFromBattle = false;
     }
 }
--- Assets/Scripts/PlayerMovement/PlayerSpawner.cs
+++ Assets/Scripts/PlayerMovement/PlayerSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
@@ -6,16 +7,30 @@ public class PlayerSpawner : MonoBehaviour
 
     void Start()
     {
+        Vector3 spawnPos = spawnPoint.position;
+
         GameObject player = Instantiate(
             CharacterSelectManager.selectedCharacterPrefab,
-            spawnPoint.position,
+            spawnPos,
             Quaternion.identity
         );
-        
+
+        // If returning from won battle, move player after one frame
+        // (one frame delay lets all Start() methods finish first)
+        if (BattleData.HasReturnPosition)
+            StartCoroutine(MovePlayerNextFrame(player));
+
         CameraFollow cam = FindObjectOfType<CameraFollow>();
         if (cam != null)
-        {
             cam.target = player.transform;
-        }
+    }
+
+    private IEnumerator MovePlayerNextFrame(GameObject player)
+    {
+        yield return null;
+        
+        Debug.Log($"PlayerSpawner moving player to {BattleData.ReturnPlayerPosition}");
+        player.transform.position = BattleData.ReturnPlayerPosition;
+        BattleData.HasReturnPosition = false;
     }
 }
