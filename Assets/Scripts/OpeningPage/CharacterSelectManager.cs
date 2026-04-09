f5c294ee6b08e0dd8e48ea018978246d17bd9b77
Kabi272 <143365355+Kabi272@users.noreply.github.com>
2026-04-08 21:10:08 -0400

    Character Screen goes to SmugglersIsland

--- Assets/Scripts/OpeningPage/CharacterSelectManager.cs
+++ Assets/Scripts/OpeningPage/CharacterSelectManager.cs
@@ -18,6 +18,6 @@ public class CharacterSelectManager : MonoBehaviour
 
         Debug.Log("Selected: " + selectedCharacter);
 
-        SceneManager.LoadScene("BlackwaterFlagship");
+        SceneManager.LoadScene("SmugglersIsland");
     }
 }
